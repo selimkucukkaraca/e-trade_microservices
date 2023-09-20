074347a6463372fbcc6ff87a4c34454ac349d15c
selimkucukkaraca <selimkkkaraca@hotmail.com>
2023-09-20 22:56:17 +0300

    Initial commit

---
+++ e-trade/eureka-server/src/test/java/com/selim/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selim.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

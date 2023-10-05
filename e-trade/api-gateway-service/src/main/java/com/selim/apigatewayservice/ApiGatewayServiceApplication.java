3015c3253fb450247d9df4a93c01c6a512b0e1e3
selimkucukkaraca <selimkkkaraca@hotmail.com>
2023-10-05 18:13:18 +0300

    api gateway update

--- e-trade/api-gateway-service/src/main/java/com/selim/apigatewayservice/ApiGatewayServiceApplication.java
+++ e-trade/api-gateway-service/src/main/java/com/selim/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,7 +2,13 @@ package com.selim.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.selim")
+@EntityScan(value = "com.selim.entity")
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
 
